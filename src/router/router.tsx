e1ab44c9229a750b62cf8deba1452e1fc533664e
RicardoTrejoSanjuan <trejosanjuanricardo@gmail.com>
2025-04-25 15:06:17 -0700

    fix: Remove unused import

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from 'react-router-dom';
-import { Root } from '../Root';
 import { DashboardLayout } from '../layouts';
 import { DashboardPage } from '../pages/dashboard/DashboardPage';
 import { TaskPage } from '../pages/task/TaskPage';
